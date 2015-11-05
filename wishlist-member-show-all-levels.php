3c83c788ecb0f29c766b7165fc31568e615faa63
Andrew Minion <andrew@andrewrminion.com>
2015-11-05 11:52:56 -0500

    add support for ignoring pages and hiding the header

--- wishlist-member-show-all-levels.php
+++ wishlist-member-show-all-levels.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/macbookandrew/wishlist-member-show-all-levels
  * GitHub Plugin URI: https://github.com/macbookandrew/wishlist-member-show-all-levels
  * Description: Provides a shortcode that outputs all levels a member is allowed to access
- * Version: 1.0
+ * Version: 1.1
  * Author: AndrewRMinion Design
  * Author URI: http://andrewrminion.com/
  * License: GPL2
@@ -27,7 +27,13 @@
 /**
  * Show WishListMember content on the dashboard page with Woocommerce data
  */
-function wlmsal_show_authorized_levels() {
+function wlmsal_show_authorized_levels( $atts ) {
+    // get attributes
+    $attributes = shortcode_atts(
+        'show_header'       => 'true',
+        'pages_to_ignore'   => array()
+    );
+
     // get all levels this user is authorized for
     $authorized_levels = wlmapi_get_member_levels( get_current_user_id() );
 
@@ -37,20 +43,29 @@ function wlmsal_show_authorized_levels() {
     // loop over authorized levels, displaying all content for each
     foreach ( $authorized_levels as $level ) {
 
-        $shortcode_output .= '<div class="wishlist-member-levels">
-        <h2>' . $level->Name . '</h2>
-        <ul>';
+        $shortcode_output .= '<div class="wishlist-member-levels">';
+
+        // show level header
+        if ( $attributes['show_header'] ) {
+            $shortcode_output .= '<h2>' . $level->Name . '</h2>';
+        }
+
+        // start list output
+        $shortcode_output .= '<ul>';
 
         // get all pages and posts for this level
         $this_level_pages = wlmapi_get_level_pages( $level->Level_ID );
 
         // loop over all pages for this level
         foreach ( $this_level_pages['pages']['page'] as $this_page ) {
-            $shortcode_output .= '<li><a href="' . esc_url( get_permalink( $this_page['ID'] ) ) . '">' . $this_page['name'] . '</a></li>';
+            if ( ! in_array( $this_page['ID'], $attributes['pages_to_ignore'] ) ) {
+                $shortcode_output .= '<li><a href="' . esc_url( get_permalink( $this_page['ID'] ) ) . '">' . $this_page['name'] . '</a></li>';
+            }
         }
         $shortcode_output .= '</ul>';
     }
 
+    // return all the content
     return $shortcode_output;
 }
 add_shortcode( 'wlm_all_authorized_levels', 'wlmsal_show_authorized_levels' );
